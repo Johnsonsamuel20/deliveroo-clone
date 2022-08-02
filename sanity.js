63f94e68e0ce63bc63e8a4d58d2961a3576ead43
Samuel Johnson <Johnsonsamuel324@gmail.com>
2022-08-02 17:19:42 -0500

    Added Sanity

--- sanity.js
+++ sanity.js
@@ -1,11 +1,11 @@
-import { SanityClient } from "@sanity/client";
-import { ImageUrlBuilder } from "@sanity/image-url";
+import sanityClient from "@sanity/client";
+import { imageUrlBuilder } from "@sanity/image-url";
 
 const client = sanityClient({
   projectId: "zs5hspbn",
   dataset: "production",
   useCdn: true,
-  apiVersion: "2022-07-29",
+  apiVersion: "2021-10-21",
 });
 
 const builder = imageUrlBuilder(client);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,7 +7,7 @@ import {
   TextInput,
   View,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
@@ -17,15 +17,29 @@ import {
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+
+  useEffect(() => {
+    sanityClient.fetch(`
+    *[_type == "featured"] {
+      ...,
+      restaurants[] ->{
+      dishes[]->,
+      }
+    }`);
+  }, []);
+
+  console.log(featuredCategories);
 
   return (
     <SafeAreaView className="bg-white pt-5">
